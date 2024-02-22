e4c0f0a56b61f5112d63af8ea7b1447aa8fc12fb
SazonovOleksandr <Sazonov.Oleksandr@utm.md>
2024-02-22 14:39:00 +0200

    Add controllers for pages

---
+++ SimpleFood.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleFood.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Catalog()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+        
+        public ActionResult Product()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+    }
+}
